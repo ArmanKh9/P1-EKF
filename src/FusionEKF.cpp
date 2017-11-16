ca8fba172170b44e8527fb312cb4d6e39ac835fb
ArmanKh9 <khalighi.ar9@gmail.com>
2017-11-16 18:32:07 -0500

    4

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -141,8 +141,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   		      0, 0, 0, 1;
 
   // Set the process covariance matrix Q
-  noise_ax = 9;
-  noise_ay = 9;
+  float noise_ax = 9;
+  float noise_ay = 9;
 	ekf_.Q_ = MatrixXd(4, 4);
 	ekf_.Q_ <<  dt_4 / 4*noise_ax, 0, dt_3 / 2 * noise_ax, 0,
     			   0, dt_4 / 4 * noise_ay, 0, dt_3 / 2 * noise_ay,
