cc4c5f4670243ec5072a5dc70a6961c154037e11
ArmanKh9 <khalighi.ar9@gmail.com>
2017-11-18 11:12:06 -0500

    15

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -166,15 +166,19 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.H_ = Hj_;
     ekf_.R_ = R_radar_;
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+    cout << "RADAR x_ = " << ekf_.x_ << endl;
+    cout << "Radar P_ = " << ekf_.P_ << endl;
   } else {
     // Laser updates
     ekf_.H_ = H_laser_;
     ekf_.R_ = R_laser_;
     ekf_.Update(measurement_pack.raw_measurements_);
+    cout << "Laser x_ = " << ekf_.x_ << endl;
+    cout << "Laser P_ = " << ekf_.P_ << endl;
   }
 
   // print the output
-  cout << "x_ = " << ekf_.x_ << endl;
-  cout << "P_ = " << ekf_.P_ << endl;
+  //cout << "P_ = " << ekf_.P_ << endl;
+  //cout << "RADAR x_ = " << ekf_.x_ << endl;
 
 }
