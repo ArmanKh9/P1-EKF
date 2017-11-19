937a1ad875c7b16533502f478cf049cd13d919ae
ArmanKh9 <khalighi.ar9@gmail.com>
2017-11-18 23:15:02 -0500

    24

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -68,6 +68,12 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   VectorXd z_pred(3);
   z_pred << ro, phi, ro_dot;
   VectorXd y = z - z_pred;
+  if (y(1) > 2*M_PI) {
+    y(1) = y(1) - 2*M_PI;
+  }
+  if (y(1) < -2*M_PI) {
+    y(1) = y(1) + 2*M_PI;
+  }
   MatrixXd Ht = H_.transpose();
   MatrixXd S = H_ * P_ * Ht + R_;
   MatrixXd Si = S.inverse();
